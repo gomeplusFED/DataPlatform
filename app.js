879f1540ecf36770a4dc8941cd6d0b8b5a7115d2
luoye <842891024@qq.com>
2016-03-30 11:35:43 +0800

    目录结构调整

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ var mysql = require('./models/mysql');
 var orm = require('orm');
 
 
-orm.settings.set("connection.pool",true);
+orm.settings.set("connection.pool", true);
 Object.keys(config).forEach(function(key) {
-  app.locals[key] = config[key];
+    app.locals[key] = config[key];
 });
 
-app.use(function(req, res, next){
-  if(req.headers['user-agent'].indexOf('Chrome') === -1 ) {
-    res.send('请使用谷歌浏览器');
-  } else {
-    next();
-  }
+app.use(function(req, res, next) {
+    if (req.headers['user-agent'].indexOf('Chrome') === -1) {
+        res.send('请使用谷歌浏览器');
+    } else {
+        next();
+    }
 });
 
 app.engine('html', ejs.renderFile);
@@ -34,24 +34,24 @@ app.set('view engine', 'html');
 
 //parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 //  
 //parse application/json 
 app.use(bodyParser.json());
 
-app.set('trust proxy',1);
+app.set('trust proxy', 1);
 
 app.use(session({
-  name: 'DataPlatform',
-  secret:'DataPlatform'
+    name: 'DataPlatform',
+    secret: 'DataPlatform'
 }));
 
 app.use(flash);
 
-app.use(function(req,res,next){
-  res.locals.session = req.session;
-  next();
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
 });
 
 //载入view helps
@@ -60,24 +60,28 @@ require('./helps')(app);
 new mysql(app);
 
 routers.forEach(function(router) {
-  app.use(router);
+    app.use(router);
 });
 
 app.use(lactate.static(__dirname + '/publics'));
 
-app.use(function(){
-  var args = arguments;
-  var isErr = args[0] instanceof Error;
-  if(isErr){
-    args[2].status(500).send(args[0]); 
-  }else{
-    args[2](); 
-  }
+app.use(function() {
+    var args = arguments;
+    var isErr = args[0] instanceof Error;
+    if (isErr) {
+        args[2].status(500).send(args[0]);
+    } else {
+        args[2]();
+    }
 });
 
 app.use("/dataOverview", (req, res, next) => {
-  res.render("table");
+    res.render("table");
 });
+
+app.get('/viewtest', (req, res, next) => {
+    res.render('main/index.html')
+})
 
 //app.use((err, req, res, next) => {
 //  res.render('include/404');
