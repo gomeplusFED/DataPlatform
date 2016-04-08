515069eb3194f25c2d5efd60fe27b3dcd61fcf7b
luoye <842891024@qq.com>
2016-04-08 17:40:56 +0800

    级联菜单完成

--- static/src/js/component/filterSelect.vue
+++ static/src/js/component/filterSelect.vue
@@ -1,15 +1,21 @@
 <template>
 	<div class="filter_select" v-show="pageComponentsData[componentType].length > 0">
-		<div class="filter_group" :id="'filter_group_' + index + '_' + $index" v-for="item in pageComponentsData[componentType]" track-by="$index">
+		<div class="filter_group" :id="'filter_group_' + index + '_' + $index" v-for="( pageComponentsDataIndex, item) in pageComponentsData[componentType]" track-by="$index">
 			<div class="group">
 				<strong>{{item.title}}{{item.title === '' ? '' : '：'}}</strong>
 				<div class="btn_group" v-if="!isCell">
 					<button v-for="group in item.groups" @click="getArgv(item.filter_key,group.key,$event)" track-by="$index">{{group.value}}</button>
 				</div>
 				<div class="btn_group" v-else>
-					<select :value="item.groups[0].key" v-model="selected" @change="extraSelect($event)">
+					<select @change="extraSelect($event,pageComponentsDataIndex)">
 						<option v-for="group in item.groups" :value="group.key">{{group.value}}</option>
 					</select>
+					<div class="cell_select" v-for="cell in cellData" :index="$index" v-show="checkedOption === $index">
+						<strong>{{cell.title}}{{cell.title === '' ? '' : '：'}}</strong>
+						<select @change="cellSelectChange($event,pageComponentsDataIndex)">
+							<option v-for="cellOption in cell.groups" :value="cellOption.key">{{cellOption.value}}</option>
+						</select>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -31,6 +37,8 @@
 .filter_select .filter_group .group .btn_group button.active{background: #3389d4;border: 1px solid #3389d4;color: #fff;}
 .filter_select .filter_group .group .btn_group select{font-size: 12px;outline: none;padding: 0;margin: 0;display: inline-block;vertical-align: middle;height: 27px;line-height: 27px;color: #555;background-color: #fff;    border: 1px solid #ccc;border-radius: 4px;-webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);}
 .filter_select .filter_group .group .btn_group select option{font-size: 12px;}
+.filter_select .filter_group .group .btn_group .cell_select{display: inline-block;vertical-align: middle;margin-left: 25px;}
+.filter_select .filter_group .group .btn_group .cell_select:first-child{margin-left: 0;}
 </style>
 
 <script>
@@ -43,7 +51,11 @@ var FilterSelect = Vue.extend({
 	data: function(){
 		return {
 			targetParentGroup: null,
-			isCell: false
+			isCell: false,
+			cellData: [],
+			isFirstHandler: true,
+			checkedOption: 0,
+			cellCheckedOption: 0
 		}
 	},
 	props: ['index','pageComponentsData','componentType','argvs','initData'],
@@ -54,8 +66,15 @@ var FilterSelect = Vue.extend({
 		getArgv: function(key,value,ev){
 			Vue.set(this.argvs, key, value);
 		},
-		extraSelect: function(ev){
-			console.info(this.selected);
+		extraSelect: function(ev,pageComponentsDataIndex){
+			this.checkedOption = $(ev.target).find('option:selected').index();
+			// 嵌套层级较深，从原始属性里慢慢分析吧。。。心累
+			Vue.set(this.argvs, this.pageComponentsData[this.componentType][pageComponentsDataIndex].filter_key, this.pageComponentsData[this.componentType][pageComponentsDataIndex].groups[this.checkedOption].key);
+			Vue.set(this.argvs, this.pageComponentsData[this.componentType][pageComponentsDataIndex].groups[this.checkedOption].cell.filter_key, this.pageComponentsData[this.componentType][pageComponentsDataIndex].groups[this.checkedOption].cell.groups[0].key);
+		},
+		cellSelectChange: function(ev,pageComponentsDataIndex){
+			this.cellCheckedOption = $(ev.target).find('option:selected').index();
+			Vue.set(this.argvs, this.pageComponentsData[this.componentType][pageComponentsDataIndex].groups[this.checkedOption].cell.filter_key, this.pageComponentsData[this.componentType][pageComponentsDataIndex].groups[this.checkedOption].cell.groups[this.cellCheckedOption].key);
 		},
 		checkHasCell: function(){
 			for(var item of this.pageComponentsData[this.componentType]){
@@ -63,6 +82,7 @@ var FilterSelect = Vue.extend({
 				for(var group of item.groups){
 					if(group.cell && group.cell.groups.length){
 						_count ++;
+						this.cellData.push(group.cell);
 						if(_count === item.groups.length){
 							this.isCell = true;
 						}
@@ -82,19 +102,23 @@ var FilterSelect = Vue.extend({
 				// 检测filter是否是级联选择
 				this.checkHasCell();
 
-				// 纯按钮事件绑定
-				for(var i = 0;i < this.pageComponentsData[this.componentType].length;i++){
-					(function(filterIndex){
-						$('#filter_group_' + _this.index + '_' + filterIndex).find('button').bind('click',function(){
-							$('#filter_group_' + _this.index + '_' + filterIndex).find('button').removeClass('active');
-							$(this).addClass('active');
-						})
-					})(i)
-					$('#filter_group_' + this.index + '_' + i).find('button').eq(0).trigger('click');
+				// 纯按钮事件处理
+				if(this.isFirstHandler && !this.isCell){
+					for(var i = 0;i < this.pageComponentsData[this.componentType].length;i++){
+						(function(filterIndex){
+							$('#filter_group_' + _this.index + '_' + filterIndex).find('button').bind('click',function(){
+								$('#filter_group_' + _this.index + '_' + filterIndex).find('button').removeClass('active');
+								$(this).addClass('active');
+							})
+						})(i)
+						$('#filter_group_' + this.index + '_' + i).find('button').eq(0).trigger('click');
+					}
 				}
-
-				// 级联菜单事件绑定
-				
+				// 级联菜单事件处理
+				if(this.isFirstHandler && this.isCell){
+					Vue.set(this.argvs, this.pageComponentsData[this.componentType][0].filter_key, this.pageComponentsData[this.componentType][0].groups[0].key);
+					Vue.set(this.argvs, this.pageComponentsData[this.componentType][0].groups[0].cell.filter_key, this.pageComponentsData[this.componentType][0].groups[0].cell.groups[0].key);
+				}
 
 			},
 			deep: true
