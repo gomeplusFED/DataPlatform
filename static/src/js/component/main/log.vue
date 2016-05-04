2f24a7805ce0fe46ba585462f24e63422a4e65fa
luoye <842891024@qq.com>
2016-05-04 17:56:40 +0800

    add log.vue

---
+++ static/src/js/component/main/log.vue
@@ -0,0 +1,27 @@
+<template>
+
+
+
+
+</template>
+
+
+<script>
+
+var Vue = require('Vue');
+
+var Log = Vue.extend({
+	name: 'Log',
+	data: function(){
+		return {
+
+		}
+	},
+	created: function(){
+		
+	}
+})
+
+module.exports = Log;
+
+</script>
