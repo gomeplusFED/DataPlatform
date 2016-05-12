a6ea0e6b1d46cd740f46d387c28e72607e6ac17c
yanglei <247521776@qq.com>
2016-05-12 17:42:27 +0800

    销售业绩-商品分析

--- filters/achievements/product.js
+++ filters/achievements/product.js
@@ -7,6 +7,19 @@
 module.exports = {
     productOne(data) {
         var source = data.data,
-            newData = [];
+            newData = {
+                one : 0,
+                two : 0,
+                three : 0,
+                four : 0,
+                five : 0,
+                six : 0,
+                seven : 0
+            };
+        for(var key of source) {
+            if(key.key_type === "products_acc") {
+
+            }
+        }
     }
 };
