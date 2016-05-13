e3decd4d9bbcb228c92346516b991d99e6e0f76a
Hao Sun <haosun0226@gmail.com>
2016-05-13 16:02:57 +0800

    圈子

--- filters/socialAnalysis/topicData.js
+++ filters/socialAnalysis/topicData.js
@@ -31,41 +31,50 @@ module.exports = {
             newData.topic_all_count);
         return util.toTable([[newData]], data.rows, data.cols);
     },
-    // topicsTwo(data) {
-    //     var source = data.data,
-    //         channels = util.uniq(_.pluck(source, "channel")),
-    //         total_new_topics = 0,
-    //         obj = {},
-    //         newData = [];
-    //     for(var channel of channels) {
-    //         obj[channel] = {
-    //             new_users : 0,
-    //             active_users : 0,
-    //             start_up : 0
-    //         }
-    //     }
-    //     for(var key of source) {
-    //         total_new_topics += key.new_users;
-    //         obj[key.channel].new_users += key.new_users;
-    //         obj[key.channel].active_users += key.active_users;
-    //         obj[key.channel].start_up += key.start_up;
-    //     }
-    //     for(var channel of channels) {
-    //         newData.push({
-    //             channel : channel,
-    //             new_users : obj[channel].new_users,
-    //             active_users : obj[channel].active_users,
-    //             start_up : obj[channel].start_up,
-    //             new_users_rate : util.toFixed(obj[channel].new_users, total_new_users)
-    //         });
-    //     }
-    //     return util.toTable([newData], data.rows, data.cols);
-    // },
-    // groupThree(data) {
-    //     var source = data.data,
-    //         type = "bar",
-    //         map = {}
-    // },
+    topicsTwo(data,filter_key) {
+        var source = data.data,
+            type = "line",
+            array = [{
+                key : "新增话题数",
+                value: "1"
+            }, {
+                key : "话题回复率",
+                value: "2"
+            }, {
+                key : "话题点击率",
+                value: "3"
+            }],
+            newData = {},
+            map = {};
+        map[filter_key + "_0"] = array[0].key;
+        map[filter_key + "_1"] = array[1].key;
+        map[filter_key + "_2"] = array[2].key;
+        for(var date of dates) {
+            var obj = {};
+            obj[filter_key + "_0"] = 0;
+            obj[filter_key + "_1"] = 0;
+            obj[filter_key + "_2"] = 0;
+            for(var key of source) {
+                newData[util.getDate(key.date)].value += key[filter_key];
+            }
+            newData[date] = obj;
+        }
+
+        return [{
+            type : type,
+            map : map,
+            data : newData,
+            config: { // 配置信息
+                stack: false, // 图的堆叠
+                categoryY : false //柱状图竖着
+            }
+        }];
+    },
+    groupThree(data) {
+        var source = data.data,
+            type = "bar",
+            map = {}
+    },
     topicsFour(data) {
         var source = data.data,
             newData = [],
