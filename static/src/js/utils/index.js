ef291b7ddb2a65697492db6a9f9c282baa4379cc
luoye <842891024@qq.com>
2016-06-28 14:39:10 +0800

    use-select none

--- static/src/js/utils/index.js
+++ static/src/js/utils/index.js
@@ -1,22 +1,20 @@
 'use strict';
 
-
 var common = require('./common.js');
 var dom = require('./dom.js');
 
 var utils = {};
 
 utils.mixin = function(source, target) {
-    for (var i in target) {
-        if (target.hasOwnProperty(i)) {
-            source[i] = target[i];
-        }
-    }
-    return source;
-}
+	for (var i in target) {
+		if (target.hasOwnProperty(i)) {
+			source[i] = target[i];
+		}
+	}
+	return source;
+};
 
 utils.mixin(utils, common);
 utils.mixin(utils, dom);
-
 
 module.exports = utils;
