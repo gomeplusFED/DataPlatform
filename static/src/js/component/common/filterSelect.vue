62ac516a7f56cadf59fe51d45e0ccb9554adb356
luoye <842891024@qq.com>
2016-07-05 18:31:12 +0800

    eslint

--- static/src/js/component/common/filterSelect.vue
+++ static/src/js/component/common/filterSelect.vue
@@ -17,7 +17,7 @@
 						</div>
 						<div class="panel-body">
 							<ul>
-								<li v-for="multi_option in item.groups" @click="mulit_check(multi_option, item)" :class="{'active': multiCheckedOption[multi_option.key]}">{{multi_option.value}}</li>						
+								<li v-for="multi_option in item.groups" @click="mulit_check(multi_option, item)" :class="{'active': multiCheckedOption[multi_option.key]}">{{multi_option.value}}</li>
 							</ul>
 						</div>
 						<div class="panel-footer clearfix">
@@ -55,34 +55,135 @@
 		</div>
 	</div>
 </template>
-
 <style>
-.filter_select{font-size: 0;margin: 0 0 12px;display: inline-block;}
-.filter_select .filter_group{display: inline-block;vertical-align: middle;margin-right: 25px;}
-.filter_select .filter_group .group{display: inline-block;vertical-align: middle;margin-left: 25px;position: relative;}
-.filter_select .filter_group .group:first-child{margin-left: 0;}
-.filter_select .filter_group .group>strong{font-size: 12px;display: inline-block;vertical-align: middle;}
-.filter_select .filter_group .group .btn_group{font-size: 0;display: inline-block;vertical-align: middle;}
-.filter_select .filter_group .group .btn_group:hover button{border-color: #bbb;}
-.filter_select .filter_group .group .btn_group button{display: inline-block;vertical-align: middle;border: 1px solid #cacaca;color: #333;padding: 4px 12px;background: #fff;font-size: 12px;outline: none;margin-left: -1px;transition: all ease 0.2s;-webkit-transition: all ease 0.2s;}
-.filter_select .filter_group .group .btn_group button:hover{background: #f5f5f5;}
-.filter_select .filter_group .group .btn_group button:first-child{border-radius: 2px 0 0 2px;margin-left: 0;}
-.filter_select .filter_group .group .btn_group button:last-child{border-radius: 0 2px 2px 0;}
-.filter_select .filter_group .group .btn_group button.active{background: #3389d4;border: 1px solid #3389d4;color: #fff;}
-.filter_select .filter_group .group .btn_group select{font-size: 12px;outline: none;padding: 0;margin: 0;display: inline-block;vertical-align: middle;height: 27px;line-height: 27px;color: #555;background-color: #fff;    border: 1px solid #ccc;border-radius: 4px;-webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);}
-.filter_select .filter_group .group .btn_group select option{font-size: 12px;}
-.filter_select .filter_group .group .btn_group .cell_select{display: inline-block;vertical-align: middle;margin-left: 25px;}
-.filter_select .filter_group .group .btn_group .cell_select:first-child{margin-left: 0;}
+.filter_select {
+	font-size: 0;
+	margin: 0 0 12px;
+	display: inline-block;
+}
 
-.multi_option_show .panel-body ul{overflow-y: auto;border: 1px solid #ddd;border-radius: 10px;height: 408px;}
-.multi_option_show .panel-body ul li{cursor: pointer;background: #fff;transition: all ease 0.2s;-webkit-transition: all ease 0.2s;padding: 0 8px;line-height: 32px;}
-.multi_option_show .panel-body ul li:hover{background: rgba(0,0,0,0.1);}
-.multi_option_show .panel-body ul li.active{background: #3389d4;color: #fff;}
+.filter_select .filter_group {
+	display: inline-block;
+	vertical-align: middle;
+	margin-right: 25px;
+}
 
+.filter_select .filter_group .group {
+	display: inline-block;
+	vertical-align: middle;
+	margin-left: 25px;
+	position: relative;
+}
+
+.filter_select .filter_group .group:first-child {
+	margin-left: 0;
+}
+
+.filter_select .filter_group .group>strong {
+	font-size: 12px;
+	display: inline-block;
+	vertical-align: middle;
+}
+
+.filter_select .filter_group .group .btn_group {
+	font-size: 0;
+	display: inline-block;
+	vertical-align: middle;
+}
+
+.filter_select .filter_group .group .btn_group:hover button {
+	border-color: #bbb;
+}
+
+.filter_select .filter_group .group .btn_group button {
+	display: inline-block;
+	vertical-align: middle;
+	border: 1px solid #cacaca;
+	color: #333;
+	padding: 4px 12px;
+	background: #fff;
+	font-size: 12px;
+	outline: none;
+	margin-left: -1px;
+	transition: all ease 0.2s;
+	-webkit-transition: all ease 0.2s;
+}
+
+.filter_select .filter_group .group .btn_group button:hover {
+	background: #f5f5f5;
+}
+
+.filter_select .filter_group .group .btn_group button:first-child {
+	border-radius: 2px 0 0 2px;
+	margin-left: 0;
+}
+
+.filter_select .filter_group .group .btn_group button:last-child {
+	border-radius: 0 2px 2px 0;
+}
+
+.filter_select .filter_group .group .btn_group button.active {
+	background: #3389d4;
+	border: 1px solid #3389d4;
+	color: #fff;
+}
+
+.filter_select .filter_group .group .btn_group select {
+	font-size: 12px;
+	outline: none;
+	padding: 0;
+	margin: 0;
+	display: inline-block;
+	vertical-align: middle;
+	height: 27px;
+	line-height: 27px;
+	color: #555;
+	background-color: #fff;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	-webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+}
+
+.filter_select .filter_group .group .btn_group select option {
+	font-size: 12px;
+}
+
+.filter_select .filter_group .group .btn_group .cell_select {
+	display: inline-block;
+	vertical-align: middle;
+	margin-left: 25px;
+}
+
+.filter_select .filter_group .group .btn_group .cell_select:first-child {
+	margin-left: 0;
+}
+
+.multi_option_show .panel-body ul {
+	overflow-y: auto;
+	border: 1px solid #ddd;
+	border-radius: 10px;
+	height: 408px;
+}
+
+.multi_option_show .panel-body ul li {
+	cursor: pointer;
+	background: #fff;
+	transition: all ease 0.2s;
+	-webkit-transition: all ease 0.2s;
+	padding: 0 8px;
+	line-height: 32px;
+}
+
+.multi_option_show .panel-body ul li:hover {
+	background: rgba(0, 0, 0, 0.1);
+}
+
+.multi_option_show .panel-body ul li.active {
+	background: #3389d4;
+	color: #fff;
+}
 </style>
-
 <script>
-
 /*
  * 组件说明
  * 名称：条件过滤组件
@@ -98,7 +199,7 @@ var actions = require('../../store/actions.js');
 
 var FilterSelect = Vue.extend({
 	name: 'FilterSelect',
-	data: function(){
+	data: function() {
 		return {
 			targetParentGroup: null,
 			isCell: false,
@@ -108,134 +209,132 @@ var FilterSelect = Vue.extend({
 			cellCheckedOption: 0,
 			multiConShow: false,
 			multiCheckedOption: {}
-		}
+		};
 	},
-	props: ['index','pageComponentsData','componentType','argvs','initData'],
+	props: ['index', 'pageComponentsData', 'componentType', 'argvs', 'initData'],
 	vuex: {
-	    getters: {
-	        alertConfig: function() {
-	            return store.state.alertConfig;
-	        }
-	    },
-	    actions: actions
+		getters: {
+			alertConfig: function() {
+				return store.state.alertConfig;
+			}
+		},
+		actions: actions
 	},
 	methods: {
-		getArgv: function(key,value,ev){
+		getArgv: function(key, value, ev) {
 			Vue.set(this.argvs, key, value);
 		},
-		getArgvSelect: function(key, ev){
+		getArgvSelect: function(key, ev) {
 			var value = $(ev.target).find('option:selected').val();
 			Vue.set(this.argvs, key, value);
 		},
-		extraSelect: function(ev,pageComponentsDataIndex){
+		extraSelect: function(ev, pageComponentsDataIndex) {
 			this.checkedOption = $(ev.target).find('option:selected').index();
 			// 嵌套层级较深，从原始属性里慢慢分析吧。。。心累
 			Vue.set(this.argvs, this.pageComponentsData[this.componentType][pageComponentsDataIndex].filter_key, this.pageComponentsData[this.componentType][pageComponentsDataIndex].groups[this.checkedOption].key);
 			Vue.set(this.argvs, this.pageComponentsData[this.componentType][pageComponentsDataIndex].groups[this.checkedOption].cell.filter_key, this.pageComponentsData[this.componentType][pageComponentsDataIndex].groups[this.checkedOption].cell.groups[0].key);
 		},
-		cellSelectChange: function(ev,pageComponentsDataIndex){
+		cellSelectChange: function(ev, pageComponentsDataIndex) {
 			this.cellCheckedOption = $(ev.target).find('option:selected').index();
 			Vue.set(this.argvs, this.pageComponentsData[this.componentType][pageComponentsDataIndex].groups[this.checkedOption].cell.filter_key, this.pageComponentsData[this.componentType][pageComponentsDataIndex].groups[this.checkedOption].cell.groups[this.cellCheckedOption].key);
 		},
-		checkHasCell: function(){
-			for(var item of this.pageComponentsData[this.componentType]){
+		checkHasCell: function() {
+			for (var item of this.pageComponentsData[this.componentType]) {
 				var _count = 0;
-				for(var group of item.groups){
-					if(group.cell && group.cell.groups.length){
-						_count ++;
+				for (var group of item.groups) {
+					if (group.cell && group.cell.groups.length) {
+						_count++;
 						this.cellData.push(group.cell);
-						if(_count === item.groups.length){
+						if (_count === item.groups.length) {
 							this.isCell = true;
 						}
 					}
 				}
 			}
 		},
-		mulit_check: function(item, parent){
-			if(this.multiCheckedOption[item.key]){
+		mulit_check: function(item, parent) {
+			if (this.multiCheckedOption[item.key]) {
 				var _currObj = Object.assign({}, this.multiCheckedOption);
 				delete _currObj[item.key];
 				this.multiCheckedOption = _currObj;
 				return;
 			}
-			if(Object.keys(this.multiCheckedOption).length >= parent.max){
+			if (Object.keys(this.multiCheckedOption).length >= parent.max) {
 				actions.alert(store, {
 					show: true,
 					msg: '最多可以选择' + parent.max + '个',
 					type: 'warning'
-				})
+				});
 				return;
 			}
 			Vue.set(this.multiCheckedOption, item.key, item.value);
 		},
-		applyMulti: function(item){
+		applyMulti: function(item) {
 			var _currArray = [];
-			if(this.$route.path.match(/\?(.*)/)){
-				this.$route.path.match(/\?(.*)/)[1].split('&').forEach(function(params){
-					var _curr = params.split('=')
-					if(_curr[0] === item.filter_key){
+			if (this.$route.path.match(/\?(.*)/)) {
+				this.$route.path.match(/\?(.*)/)[1].split('&').forEach(function(params) {
+					var _curr = params.split('=');
+					if (_curr[0] === item.filter_key) {
 						_currArray.push(_curr[1]);
 					}
 				});
 			}
 
-			Object.keys(this.multiCheckedOption).forEach(function(item){
+			Object.keys(this.multiCheckedOption).forEach(function(item) {
 				_currArray.push(item);
-			})
+			});
 			Vue.set(this.argvs, item.filter_key, _currArray);
 			this.multiConShow = false;
 			this.multiCheckedOption = {};
 		},
-		hideMultiCon: function(){
+		hideMultiCon: function() {
 			this.multiCheckedOption = {};
 			this.multiConShow = false;
 		},
-		selectAll: function(obj){
+		selectAll: function(obj) {
 			obj.groups.forEach((item) => {
 				Vue.set(this.multiCheckedOption, item.key, item.value);
-			})
+			});
 		}
 	},
 	watch: {
 		'pageComponentsData': {
-			handler: function(val){
+			handler: function(val) {
 				var _this = this;
 				// 异步请求组件参数，watch到变化之后初始化，其它组件类似
-				if(val === null || !this.pageComponentsData[this.componentType].length > 0){
+				if (val === null || !this.pageComponentsData[this.componentType].length > 0) {
 					return;
 				}
 				// 检测filter是否是级联选择
 				this.checkHasCell();
 
 				// 纯按钮事件处理
-				if(this.isFirstHandler && !this.isCell){
-					for(var i = 0;i < this.pageComponentsData[this.componentType].length;i++){
+				if (this.isFirstHandler && !this.isCell) {
+					for (var i = 0; i < this.pageComponentsData[this.componentType].length; i++) {
 						var _curr = this.pageComponentsData[this.componentType][i];
-						if(_curr.multi) continue;
-						if(_curr.groups.length > 5){
+						if (_curr.multi) continue;
+						if (_curr.groups.length > 5) {
 							Vue.set(this.argvs, this.pageComponentsData[this.componentType][i].filter_key, _curr.groups[0].key);
-						}else{
-							(function(filterIndex){
-								$('#filter_group_' + _this.index + '_' + filterIndex).find('button').bind('click',function(){
+						} else {
+							(function(filterIndex) {
+								$('#filter_group_' + _this.index + '_' + filterIndex).find('button').bind('click', function() {
 									$('#filter_group_' + _this.index + '_' + filterIndex).find('button').removeClass('active');
 									$(this).addClass('active');
-								})
-							})(i)
+								});
+							})(i);
 							$('#filter_group_' + this.index + '_' + i).find('button').eq(0).trigger('click');
 						}
 					}
 				}
 				// 级联菜单事件处理
-				if(this.isFirstHandler && this.isCell){
+				if (this.isFirstHandler && this.isCell) {
 					Vue.set(this.argvs, this.pageComponentsData[this.componentType][0].filter_key, this.pageComponentsData[this.componentType][0].groups[0].key);
 					Vue.set(this.argvs, this.pageComponentsData[this.componentType][0].groups[0].cell.filter_key, this.pageComponentsData[this.componentType][0].groups[0].cell.groups[0].key);
 				}
-
 			},
 			deep: true
 		}
 	}
-})
+});
 module.exports = FilterSelect;
-
 </script>
