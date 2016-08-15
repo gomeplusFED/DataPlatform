ef8f81f406983296a6a6f3cdb194d8537f792643
yanglei <247521776@qq.com>
2016-08-15 18:02:05 +0800

    辅助工具更新

--- filters/table/index.js
+++ filters/table/index.js
@@ -61,10 +61,40 @@ module.exports = {
             },{
                 caption: "删除回复数",
                 type: "number"
+            }]);rows.push(["category_id_1", "category_id_2", "new_group_num",
+                "new_topic_num", "delete_topic_num", "new_group_user_num",
+                "quit_group_user_num", "new_reply_num", "delete_reply_num"]);
+            cols.push([{
+                caption: "一级分类",
+                type: "string"
+            },{
+                caption: "二级分类",
+                type: "string"
+            },{
+                caption: "新增圈子数",
+                type: "number"
+            },{
+                caption: "新增话题数",
+                type: "number"
+            },{
+                caption: "删除话题数",
+                type: "number"
+            },{
+                caption: "新增成员次数",
+                type: "number"
+            },{
+                caption: "退圈成员数",
+                type: "number"
+            },{
+                caption: "新增回复数",
+                type: "number"
+            },{
+                caption: "删除回复数",
+                type: "number"
             }]);
         }
 
-        return util.toTable([source], rows, cols, {
+        return util.toTable([source, source], rows, cols, {
             count : [count],
             config : [merge]
         });
