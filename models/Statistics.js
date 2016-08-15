efc5e20aae9bbab5ca7fd803d415e71771288567
yanglei <247521776@qq.com>
2016-08-15 14:49:43 +0800

    加表

--- models/Statistics.js
+++ models/Statistics.js
@@ -1,3 +1,15 @@
 /**
- * Created by yanglei on 2016/8/15.
+ * @author yanglei
+ * @date 20160815
+ * @fileoverview
  */
+
+module.exports = {
+    id : {type : "number", key : true},
+    date : Date,
+    group_id : String,
+    topic_id : String,
+    group_leader_id : String,
+    key : String,
+    value : Number
+};
