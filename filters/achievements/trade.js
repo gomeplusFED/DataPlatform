306294a97d3bf9f2c8ec075d39bef30f5106ce2e
yanglei <247521776@qq.com>
2016-09-09 17:23:15 +0800

    美店

--- filters/achievements/trade.js
+++ filters/achievements/trade.js
@@ -2,96 +2,18 @@
  * Created by Hao on 2016-05-18.
  */
 
-var util = require("../../utils"),
+const util = require("../../utils"),
     moment = require("moment");
 
 module.exports = {
-    vtradeOne(data) {
-        var source = data.first.data[0],
-            now = new Date(),
-            _rows = [
+    vtradeOne(data, dates) {
+        let source = data.first.data[0],
+            newData = [];
 
-            ],
-            _cols = [
-
-            ];
-        var rows = _rows[0];
-        var cols = _cols[0];
-
-        var keyArray = rows[0];
-
-        // 昨天的数据
-        // 昨天0点 到 今天0 点
-        var qdate = convertDate(new Date(now.getTime() - 2 * 24 * 60 * 60 * 1000));
-        var zdate = convertDate(new Date(now.getTime() - 1 * 24 * 60 * 60 * 1000));
-        var jdate = convertDate(new Date(now.getTime()));
-        var zData = trimData(source, keyArray, zdate, jdate);
-
-
-
-
-        //前天的数据
-        // 前天0点 到 昨天0 点
-        var qData = trimData(source, keyArray, qdate, zdate);
-
-
-
-        //环比
-        var hb = {};
-        var hb7day ={};
-        if(!(_.isEmpty(zData))) {
-            //计算并防止累加
-            handlePrice(zData);
-
-            if(!(_.isEmpty(qData))) {
-
-                handlePrice(qData);
-
-                hb = _.clone(zData);
-                _.merge(hb, qData, function(a, b) {
-                    if (_.isNumber(b)) {
-                        return util.toFixed(a,b);
-                    }
-                });
-            }
-
-            // 7日平均环比：昨日/（average（最近7日之和））
-            // 最近7日之和: 7天前的0点 到今天 0 点
-            var date7ago = convertDate(new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000));
-            var data7day = trimData(source, keyArray, date7ago, jdate);
-            hb7day = _.clone(data7day);
-
-            _.merge(hb7day, zData, function(a, b) {
-                if (_.isNumber(a)) {
-                    return util.toFixed(b, a/7);
-                }
-            });
-            // 修正7日平均环比单价
-            handlePrice(data7day);
-            hb7day.custmer_price = util.toFixed(zData.custmer_price, data7day.custmer_price);
-            hb7day.order_price = util.toFixed(zData.order_price, data7day.order_price);
+        for(let key of data.rows[0]) {
+            let obj = {};
+            obj[key] = 0;
         }
-
-        //处理客单价和笔单价
-        // 'custmer_price', 'order_price'
-        function handlePrice(x) {
-            var amount = x.paid_amount || 0;
-            x.custmer_price = x.paid_user_num ? amount / x.paid_user_num :0;
-            x.order_price = x.paid_num ? amount / x.paid_num :0;
-        }
-
-
-
-        [zData, qData].forEach(function(x) {
-            x.custmer_price = x.custmer_price?x.custmer_price.toFixed(2):'';
-            x.order_price = x.order_price?x.order_price.toFixed(2):'';
-        });
-
-        zData.name = '昨天';
-        qData.name = '前天';
-        hb.name = '环比';
-        hb7day.name = '7日平均环比';
-        var newData = [zData, qData, hb, hb7day];
 
         return util.toTable([newData], rows, cols);
     },
