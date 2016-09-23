e1a0109d403c91f4784acc5f7e0e38e6e0eeb129
yanglei <247521776@qq.com>
2016-09-23 14:07:49 +0800

    change

--- models2/garbage/matchModule.js
+++ models2/garbage/matchModule.js
@@ -35,6 +35,34 @@ for(let item of tableArr){
     };
 }
 
+let data = `/**
+ * author@Mr.He
+ * date  @20160920
+ * content@项目中使用的数据表对应的模型名称，和其它配置
+ */
+
+
+
+/**
+ * 手动指定主键eg:
+ * "tbl_rt_useranalysis_newuser": {
+ *      "modelName": "NewAccount",
+ *      "primary"  : "channal_id"
+ *   }
+ */
+
+
+
+module.exports = {`;
+for(let key in Result) {
+    data = data.concat(`\r\t${key}:{\r\t\t`);
+    for(let k in Result[key]) {
+        data = data.concat(`${k} : "${Result[key][k]}"\r\t},`)
+    }
+}
+
+fs.writeFileSync(path.join(__dirname, "../Modules.js"), data.concat("\r};"));
+
 
 
 module.exports = {
