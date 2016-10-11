b7527e9e4356366585cfdeab39f6d2673ae4947d
yanglei <247521776@qq.com>
2016-10-11 15:48:11 +0800

    营销渠道分析

--- controllers/manage/channelAnalysis/market.js
+++ controllers/manage/channelAnalysis/market.js
@@ -11,6 +11,37 @@ module.exports = (Router) => {
         router : "/channelAnalysis/marketOne",
         modelName : ["ChaChalistChannel"],
         platform : false,
+        selectFilter(req, cb) {
+            let filter_select = [{
+                title: '指标',
+                filter_key : 'filter_key',
+                groups: [{
+                    key: 'active_pv',
+                    value: '活动页面PV'
+                }, {
+                    key: 'register',
+                    value: '活动新增注册'
+                }, {
+                    key: 'coupon_get_num',
+                    value: '优惠券领取数量'
+                }, {
+                    key: 'coupon_use_num',
+                    value: '优惠券使用数量'
+                }, {
+                    key: 'order_num',
+                    value: '订单总量'
+                }, {
+                    key: 'pay_num',
+                    value: '支付总量'
+                }, {
+                    key: 'order_num_money',
+                    value: '订单总金额'
+                }, {
+                    key: 'pay_num_money',
+                    value: '实际支付总金额'
+                }]
+            }];
+        },
         filter(data, query, dates, type) {
             return filter.shopOne(data, query.filter_key, dates);
         }
