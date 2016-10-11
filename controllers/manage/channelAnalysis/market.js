91713ed1171f4fdb77aa5652a4996e85ce7a1653
yanglei <247521776@qq.com>
2016-10-11 15:04:05 +0800

    营销渠道分析

--- controllers/manage/channelAnalysis/market.js
+++ controllers/manage/channelAnalysis/market.js
@@ -3,39 +3,14 @@
  * @date 2016-10-11
  * @fileoverview
  */
-let main = require("../../../base/main.js");
+let main = require("../../../base/main.js"),
+    filter = require("../../../filters/channelAnalysis/market");
 
 module.exports = (Router) => {
     Router = new main(Router,{
-        router : "/achievements/shopOne",
-        modelName : ["SalesPerfShopKv"],
+        router : "/channelAnalysis/marketOne",
+        modelName : ["ChaChalistChannel"],
         platform : false,
-        procedure : [{
-            aggregate : {
-                value : ["date"]
-            },
-            sum : ["xpop_shops_num_add_al", "xpop_shops_num_succ_add_al",
-                "deal_shops_num", "xpop_shops_num_acc_al"],
-            groupBy : ["date"],
-            get : ""
-        }],
-        filter_select: [{
-            title: '',
-            filter_key : 'filter_key',
-            groups: [{
-                key: 'xpop_shops_num_add_al',
-                value: '新增注册店铺'
-            }, {
-                key: 'xpop_shops_num_succ_add_al',
-                value: '成功入驻店铺'
-            }, {
-                key: 'deal_shops_num',
-                value: '成功交易店铺'
-            }, {
-                key: 'xpop_shops_num_acc_al',
-                value: '被访问店铺数'
-            }]
-        }],
         filter(data, query, dates, type) {
             return filter.shopOne(data, query.filter_key, dates);
         }
