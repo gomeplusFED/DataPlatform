cbeff0659730e00a3a7d7980f4a9d9b79e785e53
goumang2010 <goumang2010@live.com>
2016-11-01 11:03:06 +0800

    add hover effect

--- static/src/js/component/custom/databp/visualbp.vue
+++ static/src/js/component/custom/databp/visualbp.vue
@@ -1,29 +1,30 @@
 <template>
-   <div class="page-content">
-	<form class="form-inline">
-	    <div class="form-group">
+   <div class='page-content'>
+	<form class='form-inline'>
+	    <div class='form-group'>
 	        <label>埋点URL</label>
-	        <input type="text" class="form-control" placeholder="" value="{{searchData.companyName}}">
+	        <input type='text' class='form-control' placeholder='' value='{{searchData.companyName}}'>
 	    </div>
-	    <div class="form-group">
+	    <div class='form-group'>
 	        <label>平台</label>
 		    <select>
-				<option value="pc">PC</option>
-				<option value="h5">H5</option>
+				<option value='pc'>PC</option>
+				<option value='h5'>H5</option>
 			</select>
 		 </div>
-	    <button on-click="search" type="button" class="btn ent-btn-blue search-btn ">检索页面</button>
+	    <button on-click='search' type='button' class='btn ent-btn-blue search-btn '>检索页面</button>
 	</form>
         <!-- nav -->
-        <div id="container" class="main">
-	        <div class="tabpanel_content" style="width: 100%; height: 1000px;">
-		        <div class="html_content" style="z-index: 2;">
-		        	<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" id="tab_baseQuery"  src="{{url}}"></iframe>
+        <div id='container' class='main'>
+	        <div class='tabpanel_content' style='width: 100%; height: 1000px;'>
+		        <div class='html_content' style='z-index: 2;'>
+		        	<iframe frameborder='no' border='0' marginwidth='0' marginheight='0' id='tab_baseQuery'  src='{{url}}'></iframe>
 		        </div>
 	        </div>
         </div>
         
     </div>
+    <m-loading :loading.sync='loading'></m-loading>
 </template>
 <script>
 	var Vue = require('Vue');
@@ -47,13 +48,13 @@
 				initData: {},
 				url: '',
 				argvs: {
-					channel: "",
-					coupon_type: "",
+					channel: '',
+					coupon_type: '',
 					day_type: 1,
 					endTime: '',
 					startTime: '',
-					type: "new_vshop_num",
-					ver: ""
+					type: 'new_vshop_num',
+					ver: ''
 				},
 				loading: {
 					show: true,
@@ -68,6 +69,27 @@
 		ready() {
 			var _this = this;
 			this.url = '/databp/html?url=https://www.gomeplus.com/';
+			$('iframe').load(function(){
+				_this.loading.show = false;
+				var $iframe = $(this).contents();
+				var $head = $iframe.find('head'); 
+				var $body = $iframe.find('body');
+				var hovered = [];
+				$head.append('<style> .bphover {border: 3px solid blue;}</style>');
+			    $body.bind('contextmenu', function(e) {
+			  		// console.log(e.target);
+				    e.preventDefault();
+				});
+				$body.mouseover(
+					function(e) {
+						for (var i in hovered) {
+							hovered[i].removeClass('bphover');
+						}
+						var $target = $(e.target)
+					    $target.addClass('bphover');
+					    hovered.push($target);
+					});
+			});
 		},
 		methods: {
 
@@ -102,6 +124,10 @@ iframe {
     -o-transform-origin: 0 0;
     -webkit-transform: scale(0.8);
     -webkit-transform-origin: 0 0;
+}
+
+.hover {
+	border: 5px solid blue;
 }
 
 </style>
