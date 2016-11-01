261845601e6e0a433c04509b960d7eabfb898f5b
goumang2010 <goumang2010@live.com>
2016-11-01 18:04:05 +0800

    enable to choose PC or h5

--- controllers/manage/databp/index.js
+++ controllers/manage/databp/index.js
@@ -11,11 +11,29 @@ module.exports = (Router) => {
     //圈子数据总揽
     Router.get("/databp/html", (req, res, next) => {
         let url = req.query.url;
-        fetch(url)
+        let mobile = (req.query.m === '1'? true:false);
+        let options = {};
+        if (mobile) {
+            options.headers = {
+                'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
+            }
+        }
+        console.log(JSON.stringify(options,null,4));
+        fetch(url, options)
         .then(function(result) {
             return result.text();
         }).then(function(body) {
-             res.end(body);
+            let html = body;
+            // 移动端移除头部script，防止iframe无法正常渲染
+            if (mobile) {
+                html = html.replace(/^[\s\S]+?(<!DOCTYPE)/mi, function(m, p1) {
+                    return p1;
+                });
+            }
+            res.end(html);
+        }).catch(function(e) {
+            console.log(e);
+            res.end(e);
         });
 
     });
