29e8289fdba79be417b43f3fe2a1d6e832ead76a
yanglei <247521776@qq.com>
2016-11-03 18:34:32 +0800

    change

--- base/main.js
+++ base/main.js
@@ -204,9 +204,7 @@ api.prototype = {
                     show : this.control_table_col
                 },
                 global_plataform : this.global_platform,
-                toggle: {
-                    show: this.toggle
-                }
+                toggle: this.toggle
             }
         });
     },
