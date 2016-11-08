2d36813df061071143c006ac35b0d2341403eaf2
goumang2010 <goumang2010@live.com>
2016-11-08 17:21:50 +0800

    revise del api

--- static/src/js/component/custom/databp/api.js
+++ static/src/js/component/custom/databp/api.js
@@ -10,8 +10,8 @@ $.support.cors = true;
 function filterArgs(data, args) {
 	var newdata = {};
 	for(var key of args) {
-		// 不允许空字符串,0值
-		if(data[key]) {
+		// 不允许空字符串
+		if(data[key] != null && data[key] !== '') {
 			newdata[key] = data[key];
 		}
 	}
@@ -62,11 +62,11 @@ function errHandler(err) {
 var api = {
 	// {pageUrl, selector, platform, pointId, matchUrlId}
 	getBp(data) {
-		return buildAjax('/point', filterArgs(data,['pageUrl', 'selector', 'platform', 'pointId'])).then(extractResult, errHandler).catch(errHandler);
+		return buildAjax('/point', filterArgs(data, ['pageUrl', 'selector', 'platform', 'pointId'])).then(extractResult, errHandler).catch(errHandler);
 	},
 	// {pageUrl, selector, pointName, platform, pointId, matchUrlId, pattern, publicParam, privateParam}
 	updateBp(data) {
-		return buildAjax('/point', data, 'put').then(extractResult).catch(errHandler).then(function(res) {
+		return buildAjax('/point', filterArgs(data, ['pageUrl', 'selector', 'pointName', 'platform', 'pointId', 'matchUrlId', 'pattern', 'publicParam', 'privateParam']), 'put').then(extractResult).catch(errHandler).then(function(res) {
 			actions.alert(store, {
 		        show: true,
 		        msg: '更新成功',
@@ -85,8 +85,13 @@ var api = {
 		});
 	},
 	// {pointId, matchUrlId}
-	deleteBp(data) {
-		return buildAjax('/point', filterArgs(data,['pointId', 'matchUrlId']), 'delete').then(extractResult).catch(errHandler).then(function(res) {
+	deleteBp(id) {
+		return buildAjax('/point', {pointId: id}, 'delete').then(function(res) {
+			if(res.code !== '200' || res.iserror !== '0') {
+				return Promise.reject('删除失败：' + res.msg);
+			}
+			return res;
+		}).catch(errHandler).then(function(res) {
 			actions.alert(store, {
 		        show: true,
 		        msg: '删除成功',
@@ -97,158 +102,7 @@ var api = {
 	// useless: selector
 	// {pageUrl, platform, pointName, page, size}
 	listBps(data){
-				return Promise.resolve({
-		    "data": {
-		        "total": 20,
-		        "result": [{
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        }, 
-		        {
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        },
-		        {
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        },
-		        {
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        },
-		        {
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        },
-		        {
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        },
-		        {
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        },
-		        {
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        },
-		        {
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        },
-		        {
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        },
-		        {
-		            "id": 50,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>2",
-		            "pointName": "埋点2",
-		            "pointParam": "uid=1",
-		            "createTime": 1478239758000,
-		            "updateTime": 1478239758000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "1"
-		        },
-		        {
-		            "id": 51,
-		            "pageUrl": "https://mall.gomeplus.com/item/2016-11042.html",
-		            "selector": "<div/>3",
-		            "pointName": "埋点4",
-		            "pointParam": "uid=1&aid=1&cid=2016-11042&bid=2",
-		            "createTime": 1478239778000,
-		            "updateTime": 1478240238000,
-		            "platform": "H5",
-		            "isActive": "1",
-		            "uniquePoint": "0"
-		        }]
-		    },
-		    "code": "200",
-		    "msg": "",
-		    "iserror": "0"
-		}).then(function(res) {
+		return buildAjax('/pointList', data).then(function(res) {
 			if(res.code !== '200' || res.iserror !== '0') {
 				return Promise.reject('获取埋点信息失败：' + res.msg);
 			}
@@ -262,20 +116,6 @@ var api = {
 	            return Promise.reject('获取的埋点信息为空');
 			}
 		}).catch(errHandler);
-		// return buildAjax('/pointList', data).then(function(res) {
-		// 	if(res.code !== '200' || res.iserror !== '0') {
-		// 		return Promise.reject('获取埋点信息失败：' + res.msg);
-		// 	}
-		// 	var data;
-		// 	if (res && (data = res.data) && (data = data.result)) {
-		// 		return {
-		// 			data,
-		// 			total: res.data.total
-		// 		}
-		// 	} else {
-	 //            return Promise.reject('获取的埋点信息为空');
-		// 	}
-		// }).catch(errHandler);
 	}
 }
 module.exports = api;
