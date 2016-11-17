d4626415e2be812f454b664232a77676dd2ad4b4
goumang2010 <goumang2010@live.com>
2016-11-17 17:28:16 +0800

    filter xhr url

--- controllers/manage/databp/script/xhr-proxy.js
+++ controllers/manage/databp/script/xhr-proxy.js
@@ -6,7 +6,11 @@
         for (var i = 0; i < arguments.length; i++) {
             args.push(arguments[i]);
         }
-        args[1] = '/databp/ajax' + url;
+        if (/https?:\/\//.test(url)) {
+            args[1] = url;
+        } else {
+            args[1] = '/databp/ajax' + url;
+        }
         // call original open method
         return open.apply(this, args);
     }
