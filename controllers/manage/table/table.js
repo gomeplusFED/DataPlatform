988b6fd50ba62a2b481554802ba7b72c40b52711
yanglei <247521776@qq.com>
2016-11-25 15:41:53 +0800

    数据报表

---
+++ controllers/manage/table/table.js
@@ -0,0 +1,12 @@
+/**
+ * @author yanglei
+ * @date 20161125
+ * @fileoverview 数据报表
+ */
+const util = require("../../../utils"),
+    EventProxy = require("eventproxy"),
+    request = require("request");
+
+module.exports = (Router) => {
+
+};
