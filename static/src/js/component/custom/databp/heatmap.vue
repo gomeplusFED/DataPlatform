d188cc31d47580a0b50f2a6911be396b648b1db9
goumang2010 <goumang2010@live.com>
2016-11-29 14:22:08 +0800

    use API

--- static/src/js/component/custom/databp/heatmap.vue
+++ static/src/js/component/custom/databp/heatmap.vue
@@ -99,29 +99,29 @@
 		},
 		methods: {
 			init(config) {
-				let data =[{
-					"pageUrl": "https://www-pre.gomeplus.com/",
-					"selector": "body > div.wrap-box > div.circle-index-list.clearfix > div:nth-child(2) > div:nth-child(1) > div > div.list-img > a > img",
-					"pv": 5336,
-					"uv": 824
-				}, {
-					"pageUrl": "https://www-pre.gomeplus.com/",
-					"selector": "body > div.wrap-box > div.circle-index-list.clearfix > div:nth-child(3) > div:nth-child(1) > div > div.list-title > p > a",
-					"pv": 15336,
-					"uv": 1824
-				}, {
-					"pageUrl": "https://www-pre.gomeplus.com/",
-					"selector": "body > div.gome-about.gome-wrap > div.public-container > main > div.left-menu > ul > li:first-child + li +li > a",
-					"pv": 19336,
-					"uv": 1924
-				}];
-				this.data = data;
-				this.generateCanvas(data);
-				return Promise.resolve();
-				//return api.getHeatData(config).then((data) => {
-				//	this.data = data;
-				// 	this.generateCanvas(data);
-				// });
+				// let data =[{
+				// 	"pageUrl": "https://www-pre.gomeplus.com/",
+				// 	"selector": "body > div.wrap-box > div.circle-index-list.clearfix > div:nth-child(2) > div:nth-child(1) > div > div.list-img > a > img",
+				// 	"pv": 5336,
+				// 	"uv": 824
+				// }, {
+				// 	"pageUrl": "https://www-pre.gomeplus.com/",
+				// 	"selector": "body > div.wrap-box > div.circle-index-list.clearfix > div:nth-child(3) > div:nth-child(1) > div > div.list-title > p > a",
+				// 	"pv": 15336,
+				// 	"uv": 1824
+				// }, {
+				// 	"pageUrl": "https://www-pre.gomeplus.com/",
+				// 	"selector": "body > div.gome-about.gome-wrap > div.public-container > main > div.left-menu > ul > li:first-child + li +li > a",
+				// 	"pv": 19336,
+				// 	"uv": 1924
+				// }];
+				// this.data = data;
+				// this.generateCanvas(data);
+				// return Promise.resolve();
+				return api.getHeatData(config).then((data) => {
+					this.data = data;
+					this.generateCanvas(data);
+				});
 			},
 			generateCanvas(data) {
 				let _this = this;
