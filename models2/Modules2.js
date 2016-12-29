325a71fafefdf09dc71c452a4fc9865a1a088ce1
yanglei <247521776@qq.com>
2016-12-29 15:16:50 +0800

    添加备注

--- models2/Modules2.js
+++ models2/Modules2.js
@@ -145,6 +145,8 @@ module.exports = {
         modelName : "ads2_new_rebate_order_trend"
     },
     //返利   END --------------
+
+    
     //o2o店铺
     ads2_o2m_shop_trade_info : {
         modelName : "ads2_o2m_shop_trade_info"
