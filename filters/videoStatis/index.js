a4342b379c2622494b85e03e9660c89d76e57063
boom <boom@boomdeMacBook-Pro.local>
2017-02-27 11:02:27 +0800

    视频分析

--- filters/videoStatis/index.js
+++ filters/videoStatis/index.js
@@ -365,6 +365,7 @@ module.exports = {
             cols.forEach(col => {
                 data2[0][col] = source[col] || 0;
                 data2[1][col] = util.toFixedLength(source[col], source.play_num, 2);
+                data2[1]["port_succ"] = util.toFixedLength(source["port_succ"], source.start_load_num, 2);
                 data2[2][col] = Chain(source[col], source[col+'_pre']);
             })
         }
