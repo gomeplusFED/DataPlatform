64f7613b11f6066eecbfe9b7a72f881be8fa25e1
boom <boom@boomdeMacBook-Pro.local>
2017-03-22 11:31:04 +0800

    返利

--- config/apiFunction/rebate.js
+++ config/apiFunction/rebate.js
@@ -621,7 +621,11 @@ module.exports = {
         let rows = [];
         let cols = [];
         for(let i = 0, len = data.rows[0].length; i < len; i++) {
-            if(i != 6 && len < 7) {
+            if(i == 6 && len == 8) {
+                rows.push(data.rows[0][i]);
+                cols.push(data.cols[0][i]);
+            }
+            if(i != 6 && len < 10) {
                 rows.push(data.rows[0][i]);
                 cols.push(data.cols[0][i]);
             } else if(i != 7 && len > 10) {
