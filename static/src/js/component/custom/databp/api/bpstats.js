e0c518f68e5f3c305a469b1ec5552fc3ffd9e22c
goumang2010 <goumang2010@live.com>
2017-04-11 11:23:29 +0800

    bpstates version

--- static/src/js/component/custom/databp/api/bpstats.js
+++ static/src/js/component/custom/databp/api/bpstats.js
@@ -1,7 +1,7 @@
 var { filterArgs, buildAjax, extractResult, errHandler } = require('./common');
 module.exports = {
 	getHeatList(data) {
-		return buildAjax('/pointHeatList', filterArgs(data, ['pageUrl', 'platform', 'pointName', 'page', 'size', 'startTime', 'endTime', 'pattern', 'isActive', 'type'])).then(function(res) {
+		return buildAjax('/pointHeatList', filterArgs(data, ['pageUrl', 'platform', 'pointName', 'version', 'page', 'size', 'startTime', 'endTime', 'pattern', 'isActive', 'type'])).then(function(res) {
 			if (res.code !== '200' || res.iserror !== '0') {
 				return Promise.reject('获取热力列表信息失败：' + res.msg);
 			}
@@ -17,7 +17,7 @@ module.exports = {
 		}).catch(errHandler);
 	},
 	getHeatSum(data) {
-		return buildAjax('/pointHeatList/total', filterArgs(data, ['pageUrl', 'platform', 'pointName', 'page', 'size', 'startTime', 'endTime', 'pattern', 'isActive', 'type'])).then(function(res) {
+		return buildAjax('/pointHeatList/total', filterArgs(data, ['pageUrl', 'platform', 'version', 'pointName', 'page', 'size', 'startTime', 'endTime', 'pattern', 'isActive', 'type'])).then(function(res) {
 			if (res.code !== '200' || res.iserror !== '0') {
 				return Promise.reject('获取热力总计信息失败：' + res.msg);
 			}
@@ -30,7 +30,7 @@ module.exports = {
 		}).catch(errHandler);
 	},
 	getHeatDetail(data) {
-		return buildAjax('/pointHeatList/detail', filterArgs(data, ['pageUrl', 'platform', 'pointId', 'startTime', 'endTime', 'pattern', 'uniquePoint', 'isActive', 'type'])).then(function(res) {
+		return buildAjax('/pointHeatList/detail', filterArgs(data, ['pageUrl', 'platform', 'version', 'pointId', 'startTime', 'endTime', 'pattern', 'uniquePoint', 'isActive', 'type'])).then(function(res) {
 			if (res.code !== '200' || res.iserror !== '0') {
 				return Promise.reject('获取热力趋势信息失败：' + res.msg);
 			}
