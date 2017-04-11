66cde8f3aaf4132a626f577fb3a60ac3534b4521
goumang2010 <goumang2010@live.com>
2017-04-11 18:21:55 +0800

    proxy

--- static/src/js/component/custom/databp/proxy/index.js
+++ static/src/js/component/custom/databp/proxy/index.js
@@ -4,11 +4,15 @@ var url = require("url");
 var proxy = httpProxy.createProxyServer({});
 var server = http.createServer(function (req, res) {
     var urlObj = url.parse(req.url);
-    res.oldWriteHead = res.writeHead;
-    res.writeHead = function (statusCode, headers) {
-        // 设置跨域头
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.oldWriteHead(statusCode, headers);
+    var referer;
+    var localField = /localhost|127\.0\.0\.1/;
+    if((referer = req.headers.referer) && localField.test(referer)) {
+        res.oldWriteHead = res.writeHead;
+        res.writeHead = function (statusCode, headers) {
+            // 设置跨域头
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.oldWriteHead(statusCode, headers);
+        }
     }
     let target = urlObj.protocol + "//" + urlObj.host;
     proxy.web(req, res, {
