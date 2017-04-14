952c6900aadba3c3bd9ea0e4f138462bd6aed73e
boom <wo247521776@qq.com>
2017-04-14 10:46:49 +0800

    社交分析

--- filters/socialAnalysis/index2.js
+++ filters/socialAnalysis/index2.js
@@ -139,7 +139,7 @@ module.exports = {
      groupDetailFour(data, query) {
 
         var source = data.first.data,
-            source2 = data.second.data[0],
+            source2 = data.second.data,
             count = data.first.count > 100 ? 100 : data.first.count,
             ids = [];
             newData = [];
