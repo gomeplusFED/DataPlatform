e5b791f5b3c70988522762abdb988adc813638a6
goumang2010 <goumang2010@live.com>
2017-05-26 10:33:26 +0800

    databp: fix forward html

--- controllers/manage/databp/index.js
+++ controllers/manage/databp/index.js
@@ -7,10 +7,19 @@
 const forward = require('express-forward-html');
 const fs = require('fs');
 const path = require('path');
+const isMobile = (url) => /[/.](m|wap)\./.test(url);
 
 module.exports = (Router) => {
 
     forward({
+        isMobileUA(url, req) {
+            return isMobile(url) || (req.query.m === 'H5');
+        },
+        needRedirect(url, req) {
+            let mobile = isMobile(url);
+            let isSpecifedH5 = req.query.m === 'H5';
+            return (!isSpecifedH5 &&  mobile) || (!mobile && isSpecifedH5);
+        },
         filterHtml(html) {
             // 移除统计脚本
             return html.replace(/<script[\S]+?uba-sdk[\S]+?<\/script>/, '').replace(/top\.location/g, '{}');
